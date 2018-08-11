6211850d0de8628b859665721f67b9a3b7405cc0
roko <forthrightro@gmail.com>
2018-08-11 16:59:01 -0700

    added schemas for mvp

--- database/db.js
+++ database/db.js
@@ -2,17 +2,67 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('havendata.db');
 
 db.serialize(function() {
-  db.run("CREATE TABLE testAgain (info TEXT)");
+    db.run("DROP TABLE IF EXISTS user");
+    db.run("CREATE TABLE user (id INTEGER PRIMARY KEY, mediaTypes TEXT, interests TEXT, discoverable BOOLEAN)");
 
-  var stmt = db.prepare("INSERT INTO testAgain VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
+    db.run("DROP TABLE IF EXISTS media");
+    db.run("CREATE TABLE media (id INTEGER PRIMARY KEY, photoVidId INTEGER, musicId INTEGER,journalId INTEGER, FOREIGN KEY (photoVidId) REFERENCES photoVid(id), FOREIGN KEY (musicId) REFERENCES music(id),FOREIGN KEY (journalId) REFERENCES journal(id))");
+    
+    db.run("DROP TABLE IF EXISTS photoVid");
+    db.run("CREATE TABLE photoVid (id INTEGER PRIMARY KEY, title TEXT, description TEXT, file TEXT, userId INTEGER, FOREIGN KEY (userId) REFERENCES user(id))");
 
-  db.each("SELECT rowid AS id, info FROM testAgain", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
+    db.run("DROP TABLE IF EXISTS music");
+    db.run("CREATE TABLE music (id INTEGER PRIMARY KEY, title TEXT, file TEXT, userId INTEGER, FOREIGN KEY (userId) REFERENCES user(id))");
+
+    db.run("DROP TABLE IF EXISTS journal");
+    db.run("CREATE TABLE journal(id INTEGER PRIMARY KEY, title TEXT, description TEXT, file TEXT, userId INTEGER, FOREIGN KEY (userId) REFERENCES user(id))");
+
+    db.run("DROP TABLE IF EXISTS contacts");
+    db.run("CREATE TABLE contacts(id INTEGER PRIMARY KEY, name TEXT, phone INTEGER, email TEXT, emergencyContact BOOLEAN)");
+    // db.run("CREATE TABLE photoVid (
+    //     id integer primary key not null,
+    //     title TEXT,
+    //     description TEXT,
+    //     file TEXT,
+    //     userId integer NOT NULL,
+    //     FOREIGN KEY (userId) REFERENCES user(id),
+    // )");
+
+    // db.run("CREATE TABLE music (
+    //     id integer primary key not null,
+    //     title TEXT,
+    //     file TEXT,
+    //     userId integer NOT NULL,
+    //     FOREIGN KEY (userId) REFERENCES user(id),
+    // )");
+
+    // db.run("CREATE TABLE photoVid (
+    //     id integer primary key not null,
+    //     title TEXT,
+    //     description TEXT,
+    //     file TEXT,
+    //     userId integer NOT NULL,
+    //     FOREIGN KEY (userId) REFERENCES user(id),
+    // )");
+
+
+    // db.run("CREATE TABLE contacts (
+    //     id integer primary key not null,
+    //     name TEXT,
+    //     phone INTEGER,
+    //     email TEXT,
+    //     emergencyFlag BOOLEAN
+    // )");
+
+//   var stmt = db.prepare("INSERT INTO testAgain VALUES (?)");
+//   for (var i = 0; i < 10; i++) {
+//       stmt.run("Ipsum " + i);
+//   }
+//   stmt.finalize();
+
+//   db.each("SELECT rowid AS id, info FROM testAgain", function(err, row) {
+//       console.log(row.id + ": " + row.info);
+//   });
 });
 
 db.close();
