c3af100c0f5fcfbd4affc1317a5e0be80884c38a
roko <forthrightro@gmail.com>
2018-08-14 20:31:07 -0700

    does not save to db yet for some reason

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
-
+const db = require('./../database/db');
 const app = express();
 app.use(bodyParser.json());
 
@@ -10,9 +10,18 @@ app.get("*", (req, res) => {
   res.status(200).send();
 });
 
+//commented out for the time being to get journal posting working
 //placeholder post route
-app.post("*", (req, res) => {
-  res.status(200).send();
+// app.post("*", (req, res) => {
+//   res.status(200).send();
+// });
+
+//make server create an entry in journal table
+app.post('/journal', (req, res) => {
+  console.log('is this posting', req.body);
+  db.runSchema('havendata.db');
+  db.addJournalEntry(req);
+      res.sendStatus(200);
 });
 
 const server = app.listen(PORT);
