01667478fdc501a578b1941c24f25042b0c68123
Anthony King <anthonykingwheels@gmail.com>
2018-08-18 08:41:29 -0700

    WIP DO NOT MERGE experimenting w/ music player

--- client/src/components/Home/MediaTypes/Music.js
+++ client/src/components/Home/MediaTypes/Music.js
@@ -1,117 +1,147 @@
-import React from "react";
-import { AsyncStorage, Audio, Button, Text, StyleSheet, View, StatusBar } from "react-native";
-Expo.Audio.setIsEnabledAsync(true); // audio is enabled by default so we shouldn't need this
-import { MaterialIcons } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+// import React from "react";
+// import { AsyncStorage, Audio, Button, Text, StyleSheet, View, StatusBar } from "react-native";
+// Expo.Audio.setIsEnabledAsync(true); // audio is enabled by default so we shouldn't need this
+// import { MaterialIcons } from '@expo/vector-icons';
+// import { Ionicons } from '@expo/vector-icons';
 
-// to-dos
-// import npm module that will be used to play and store music
-// add a way for the user to select music to store
-// add the api necessary to store song in database and/or state
-// persist this data so that the same songs are available if user logs out/ closes app
+// // to-dos
+// // import npm module that will be used to play and store music
+// // add a way for the user to select music to store
+// // add the api necessary to store song in database and/or state
+// // persist this data so that the same songs are available if user logs out/ closes app
 
 
-export default class Music extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      orientationShown: false,
-      // for later use, upon first visit user is given an orientation
-      // so they know how to adds songs to this view
-      song: []
-    };
-  }
+// export default class Music extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       orientationShown: false,
+//       // for later use, upon first visit user is given an orientation
+//       // so they know how to adds songs to this view
+//       song: []
+//     };
+//   }
 
-  static navigationOptions = {
-    title: "Your Audio Haven"
-  };
+//   static navigationOptions = {
+//     title: "Your Audio Haven"
+//   };
 
-  playTibetan = async () => {
-    const soundObject = new Expo.Audio.Sound(); 
-    try {
-      await soundObject.loadAsync(require('../../../../assets/sounds/tibetan-singing-bowl.wav'));
-      await soundObject.playAsync();
-      console.log('song load success');
-    } catch (error) {
-      console.log('song load error');
-    }
-  }
+//   playTibetan = async () => {
+//     const soundObject = new Expo.Audio.Sound(); 
+//     try {
+//       await soundObject.loadAsync(require('../../../../assets/sounds/tibetan-singing-bowl.wav'));
+//       await soundObject.playAsync();
+//       console.log('song load success');
+//     } catch (error) {
+//       console.log('song load error');
+//     }
+//   }
 
-  playWater = async () => {
-    const soundObject = new Expo.Audio.Sound(); 
-    try {
-      await soundObject.loadAsync(require('../../../../assets/sounds/rowing-a-boat.mp3'));
-      await soundObject.playAsync();
-      console.log('song load success');
-    } catch (error) {
-      console.log('song load error');
-    }
-  }
+//   playWater = async () => {
+//     const soundObject = new Expo.Audio.Sound(); 
+//     try {
+//       await soundObject.loadAsync(require('../../../../assets/sounds/rowing-a-boat.mp3'));
+//       await soundObject.playAsync();
+//       console.log('song load success');
+//     } catch (error) {
+//       console.log('song load error');
+//     }
+//   }
 
-  playFire = async () => {
-    const soundObject = new Expo.Audio.Sound(); 
-    try {
-      await soundObject.loadAsync(require('../../../../assets/sounds/campfire.mp3'));
-      await soundObject.playAsync();
-      console.log('song load success');
-    } catch (error) {
-      console.log('song load error');
-    }
-  }
+//   playFire = async () => {
+//     const soundObject = new Expo.Audio.Sound(); 
+//     try {
+//       await soundObject.loadAsync(require('../../../../assets/sounds/campfire.mp3'));
+//       await soundObject.playAsync();
+//       console.log('song load success');
+//     } catch (error) {
+//       console.log('song load error');
+//     }
+//   }
 
-  playBodyScanMeditation = async () => {
-    const soundObject = new Expo.Audio.Sound(); 
-    try {
-      await soundObject.loadAsync(require('../../../../assets/sounds/body-scan-meditation-four-min.mp3'));
-      await soundObject.playAsync();
-      console.log('song load success');
-    } catch (error) {
-      console.log('song load error');
-    }
-  }
+//   playBodyScanMeditation = async () => {
+//     const soundObject = new Expo.Audio.Sound(); 
+//     try {
+//       await soundObject.loadAsync(require('../../../../assets/sounds/body-scan-meditation-four-min.mp3'));
+//       await soundObject.playAsync();
+//       console.log('song load success');
+//     } catch (error) {
+//       console.log('song load error');
+//     }
+//   }
 
-  playBreathingMeditation = async () => {
-    const soundObject = new Expo.Audio.Sound(); 
-    try {
-      await soundObject.loadAsync(require('../../../../assets/sounds/breathing-meditation-three-min.mp3'));
-      await soundObject.playAsync();
-      console.log('song load success');
-    } catch (error) {
-      console.log('song load error');
-    }
-  }
+//   playBreathingMeditation = async () => {
+//     const soundObject = new Expo.Audio.Sound(); 
+//     try {
+//       await soundObject.loadAsync(require('../../../../assets/sounds/breathing-meditation-three-min.mp3'));
+//       await soundObject.playAsync();
+//       console.log('song load success');
+//     } catch (error) {
+//       console.log('song load error');
+//     }
+//   }
 
+//   render() {
+//     return ( 
+//       <View style={styles.container}>
+//         <MaterialIcons name="play-circle-outline" size={48} color="red" onPress={this.playTibetan} />
+//         <Text>Tibetan Singing Bowl</Text>
+//         <MaterialIcons name="play-circle-outline" size={48} color="red" onPress={this.playWater} />
+//         <Text>Water Flowing</Text>
+//         <MaterialIcons name="play-circle-outline" size={48} color="red" onPress={this.playFire} />
+//         <Text>Campfire</Text>
+//         <MaterialIcons name="play-circle-outline" size={48} color="red" onPress={this.playBreathingMeditation} />
+//         <Text>Breathing Meditation (3m)</Text>
+//         <MaterialIcons name="play-circle-outline" size={48} color="red" onPress={this.playBodyScanMeditation} />
+//         <Text>Body Scan Meditation (4m)</Text>
+//         <Button title="I'm done, sign me out" onPress={this._signOutAsync} />
+//         <StatusBar barStyle="default" />
+//       </View>
+//     );
+//   }
+
+//   _signOutAsync = async () => {
+//     await AsyncStorage.clear();
+//     this.props.navigation.navigate("Auth");
+//   };
+// }
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
+
+// Pre-MVP player above, experimental Spotify-ish player below
+
+
+import React, { Component } from 'react';
+
+//import { AsyncStorage, Audio, Button, Text, StyleSheet, View, StatusBar } from "react-native";
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export default class Music extends Component {
   render() {
-    return ( 
+    return (
       <View style={styles.container}>
-        <MaterialIcons name="play-circle-outline" size={48} color="red" onPress={this.playTibetan} />
-        <Text>Tibetan Singing Bowl</Text>
-        <MaterialIcons name="play-circle-outline" size={48} color="red" onPress={this.playWater} />
-        <Text>Water Flowing</Text>
-        <MaterialIcons name="play-circle-outline" size={48} color="red" onPress={this.playFire} />
-        <Text>Campfire</Text>
-        <MaterialIcons name="play-circle-outline" size={48} color="red" onPress={this.playBreathingMeditation} />
-        <Text>Breathing Meditation (3m)</Text>
-        <MaterialIcons name="play-circle-outline" size={48} color="red" onPress={this.playBodyScanMeditation} />
-        <Text>Body Scan Meditation (4m)</Text>
-        <Button title="I'm done, sign me out" onPress={this._signOutAsync} />
-        <StatusBar barStyle="default" />
+        <Text style={{color: 'white'}}>
+          Hello React Native!
+        </Text>
       </View>
     );
   }
-
-  _signOutAsync = async () => {
-    await AsyncStorage.clear();
-    this.props.navigation.navigate("Auth");
-  };
 }
 
-
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+    backgroundColor: 'rgb(4,4,4)',
+  },
+}
 
--- tests/db/db.unit.test.js
+++ tests/db/db.unit.test.js
@@ -1,21 +1,21 @@
-// makes the mocked database accessible to these tests
 const imports = require('../../database/db');
 const runSchema = imports.runSchema;
 var sqlite3 = require('sqlite3').verbose();
 var testDB = new sqlite3.Database(':memory');
 
-// runSchema function contains all schema from database file so that test db generated will have identical structure to 
-// application database
+// runSchema function contains all schema from database file so that test db generated will have 
+// identical structure to application database
 const createDatabaseForTest = () => {
   runSchema(testDB);
 };  
 
 // the test suite below leaves a lot to be desired, obviously. I'm leaving it as is for now 
 // I have not figured out how to access the database within the test functions so that more 
-// meaningful tests can be run. Will schedule office hours with mentor tomorrow if
-// help desk ticket doesn't fix the problem
+// meaningful tests can be run. Will schedule office hours with tech mentor 
 describe('database', () => {
   it('exists', () => {
+    testDB.run('.open :memory')
+    console.log(testDB);
     expect(testDB).toBeTruthy();
     expect(testDB.filename).toEqual(':memory');
   })
