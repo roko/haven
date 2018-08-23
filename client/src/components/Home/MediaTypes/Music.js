5d0e279ce8759271f5a74085b32e2b017efc8df2
Anthony King <anthonykingwheels@gmail.com>
2018-08-23 11:46:15 -0700

    Updated music artwork to match audio tracks

--- client/src/components/Home/MediaTypes/Music.js
+++ client/src/components/Home/MediaTypes/Music.js
@@ -53,17 +53,17 @@ const PLAYLIST = [
 	new PlaylistItem(
 		'Comfort Fit - “Sorry”',
 		'https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Comfort_Fit_-_03_-_Sorry.mp3',
-		'https://www.thestudentplaylist.com/wp-content/uploads/2018/06/public_enemy_it_takes_a_nation_of_millions_to_hold_us_back.jpg'
+		'https://images.shazam.com/coverart/t55537491-b955650891_s400.jpg'
 	),
 	new PlaylistItem(
 		'Mildred Bailey – “All Of Me”',
 		'https://ia800304.us.archive.org/34/items/PaulWhitemanwithMildredBailey/PaulWhitemanwithMildredBailey-AllofMe.mp3',
-		'https://upload.wikimedia.org/wikipedia/commons/c/c9/Drake_-_Hotline_Bling.png'
+		'https://tinyurl.com/y9u8e437'
 	),
 	new PlaylistItem(
 		'Podington Bear - “Rubber Robot”',
 		'https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Podington_Bear_-_Rubber_Robot.mp3',
-		'https://upload.wikimedia.org/wikipedia/commons/c/c9/Drake_-_Hotline_Bling.png'
+		'https://cdn-img.jamendo.com/albums/s166/166372/covers/1.300.jpg?du=1529657778'
 	),
 ];
 
