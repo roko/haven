a6cfaf5195c9810a04fae6e8f238fef3fd36337b
Maureen and John McCurley <maureenandjohn@Maureens-iMac.local>
2018-08-31 04:15:58 -0400

    New photo library and styling

--- client/src/components/Home/MediaTypes/PhotoVideo/PhotoLanding.js
+++ client/src/components/Home/MediaTypes/PhotoVideo/PhotoLanding.js
@@ -1,8 +1,30 @@
-const coffee = require('../photoStock/coffee.jpg');
-const drive = require("../photoStock/drive.jpg");
-const house = require("../photoStock/house.jpg");
-const train = require("../photoStock/train.jpg");
-export default photoData = [ 
+const coffee = require('../photoStock/0Wedding1.jpeg');
+const drive = require("../photoStock/1Friends.jpeg");
+const house = require("../photoStock/3Family.jpeg");
+const train = require("../photoStock/4Proposal.jpg");
+
+const zero = require("../PhotoGridForHaven/0.jpg");
+const one  = require("../PhotoGridForHaven/1.jpg");
+const two = require("../PhotoGridForHaven/2.jpeg");
+const three = require("../PhotoGridForHaven/3.jpg");
+const four = require("../PhotoGridForHaven/4.jpg");
+const five = require("../PhotoGridForHaven/5.jpg");
+const six = require("../PhotoGridForHaven/6.jpg");
+const seven = require("../PhotoGridForHaven/7.jpg");
+const eight = require("../PhotoGridForHaven/8.jpg");
+const nine = require("../PhotoGridForHaven/9.jpg");
+const ten = require("../PhotoGridForHaven/10.jpg");
+const eleven = require("../PhotoGridForHaven/11.jpeg");
+const twelve  = require("../PhotoGridForHaven/12.jpeg");
+const thirteen = require("../PhotoGridForHaven/13.jpeg");
+const fourteen  = require("../PhotoGridForHaven/14.jpg");
+const fifteen = require("../PhotoGridForHaven/15.jpg");
+const sixteen = require("../PhotoGridForHaven/16.jpg");
+const seventeen = require("../PhotoGridForHaven/17.jpg");
+const eighteen = require("../PhotoGridForHaven/18.jpg");
+const nineteen = require("../PhotoGridForHaven/19.jpg");
+
+export const photoData = [ 
     {
     "story": "Favorite Coffee Shop from Home :)",
     "location": "Austin, TX",
@@ -23,4 +45,102 @@ export default photoData = [
     "location": "China",
     "image": train
     },
+]
+
+export const GridData = [
+       {
+        "story": "Favorite Coffee Shop from Home :)",
+        "location": "Austin, TX",
+        "image": one
+        },
+        {
+        "story": "Driving with Amelia",
+        "location": "Richmond, VA",
+        "image": two
+        }, 
+        {
+        "story": "Story 3",
+        "location": "Iceland",
+        "image": three
+        },
+        {
+        "story": "Story 4",
+        "location": "China",
+        "image": four
+        },
+        {
+        "story": "Favorite Coffee Shop from Home :)",
+        "location": "Austin, TX",
+        "image": five
+        },
+        {
+        "story": "Driving with Amelia",
+        "location": "Richmond, VA",
+        "image": six
+        }, 
+        {
+        "story": "Story 3",
+        "location": "Iceland",
+        "image": seven
+        },
+        {
+        "story": "Story 4",
+        "location": "China",
+        "image": eight
+        },
+        {
+        "story": "Story 4",
+        "location": "China",
+        "image": nine
+         },
+         {
+         "story": "Favorite Coffee Shop from Home :)",
+         "location": "Austin, TX",
+         "image": ten
+         },
+         {
+         "story": "Driving with Amelia",
+         "location": "Richmond, VA",
+         "image": eleven
+         }, 
+         {
+         "story": "Story 3",
+         "location": "Iceland",
+         "image": twelve
+         },
+         {
+         "story": "Story 4",
+         "location": "China",
+         "image": thirteen
+         },
+         {
+         "story": "Story 4",
+         "location": "China",
+         "image": fourteen
+          },
+          {
+          "story": "Driving with Amelia",
+          "location": "Richmond, VA",
+          "image": fifteen
+          }, 
+          {
+          "story": "Story 3",
+          "location": "Iceland",
+          "image": sixteen
+          },
+          {
+          "story": "Story 4",
+          "location": "China",
+          "image": seventeen
+          },
+          {
+          "story": "Story 4",
+          "location": "China",
+          "image": eighteen
+           },
+           {
+            "story": "Story 4",
+            "location": "China",
+            "image": nineteen
+             },
 ]
