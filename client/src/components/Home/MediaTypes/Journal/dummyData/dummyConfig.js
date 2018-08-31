e8f08dd95e46abe21bb27f4cfcc9df3b3a9881c6
roko <forthrightro@gmail.com>
2018-08-31 00:03:41 -0700

    save minor styling changes so I can rebase

--- client/src/components/Home/MediaTypes/Journal/dummyData/dummyConfig.js
+++ client/src/components/Home/MediaTypes/Journal/dummyData/dummyConfig.js
@@ -1,6 +1,6 @@
 //update with your router ip to get the journal functionality working properly
 const journalEndpoint = {
-  fetch: "http://192.168.1.132"
+  fetch: "http://192.168.0.106"
 };
 
 module.exports.journalEndpoint = journalEndpoint;
