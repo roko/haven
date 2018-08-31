2460e4810a9c9fa324be90d93c3046e4ae79ff02
chrisamurao <freshpotatoes@gmail.com>
2018-08-30 21:15:38 -0700

    style fixes

--- client/src/components/Home/MediaTypes/PhotoVideo/PhotoLanding.js
+++ client/src/components/Home/MediaTypes/PhotoVideo/PhotoLanding.js
@@ -1,29 +1,26 @@
-const coffee = require('../photoStock/coffee.jpg')
+const coffee = require('../photoStock/coffee.jpg');
 const drive = require("../photoStock/drive.jpg");
 const house = require("../photoStock/house.jpg");
 const train = require("../photoStock/train.jpg");
-
-export const photoData = [ 
+export default photoData = [ 
     {
     "story": "Favorite Coffee Shop from Home :)",
     "location": "Austin, TX",
-     "image": coffee
+    "image": coffee
     },
     {
     "story": "Driving with Amelia",
     "location": "Richmond, VA",
-      "image": drive
+    "image": drive
     }, 
     {
     "story": "Story 3",
     "location": "Iceland",
-      "image": house
+    "image": house
     },
     {
     "story": "Story 4",
     "location": "China",
-      "image": train
-
+    "image": train
     },
 ]
-export default photoData 
